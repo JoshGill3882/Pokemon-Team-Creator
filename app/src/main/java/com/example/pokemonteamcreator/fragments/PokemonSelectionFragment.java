7e064c5e3a2a40d4e7d409eb206f7e05dccc9d5f
JoshGill3882 <joshgill2013@gmail.com>
2022-06-17 10:14:09 +0100

    Formatting to make method more readable

--- app/src/main/java/com/example/pokemonteamcreator/fragments/PokemonSelectionFragment.java
+++ app/src/main/java/com/example/pokemonteamcreator/fragments/PokemonSelectionFragment.java
@@ -37,52 +37,52 @@ public class PokemonSelectionFragment extends Fragment {
         // If the enter key is pressed on the edit text
         // Gained from - http://androidsbs.blogspot.com/2013/05/perform-action-after-enter-keypress-on.html
         editText.setOnKeyListener(
-                (view, keyCode, keyEvent) -> {
-                    if ((keyEvent.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
-                        Pokemon pokemonSelected = selectPokemon(editText.getText().toString().toLowerCase());
+            (view, keyCode, keyEvent) -> {
+                if ((keyEvent.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    Pokemon pokemonSelected = selectPokemon(editText.getText().toString().toLowerCase());
 
-                        if (pokemonSelected == null) {
-                            Toast.makeText(getContext(), "Pokemon not found", Toast.LENGTH_LONG).show();
-                        } else {
-                            System.out.println(pokemonSelected.getName());
-                            System.out.println(Arrays.toString(pokemonSelected.getTypes()));
-                            System.out.println(pokemonSelected.getImageURL());
+                    if (pokemonSelected == null) {
+                        Toast.makeText(getContext(), "Pokemon not found", Toast.LENGTH_LONG).show();
+                    } else {
+                        System.out.println(pokemonSelected.getName());
+                        System.out.println(Arrays.toString(pokemonSelected.getTypes()));
+                        System.out.println(pokemonSelected.getImageURL());
 
-                            String displayName =
-                                    pokemonSelected.getName().substring(0,1).toUpperCase()
-                                            + pokemonSelected.getName().substring(1);
-                            editText.setText(displayName);
+                        String displayName =
+                            pokemonSelected.getName().substring(0,1).toUpperCase()
+                                + pokemonSelected.getName().substring(1);
+                        editText.setText(displayName);
 
-                            TextView typeText1 = view1.findViewById(R.id.fragmentTypeText1);
-                            typeText1.setText(pokemonSelected.getTypes()[0].name());
-                            if (pokemonSelected.getTypes()[1] != null) {
-                                TextView typeText2 = view1.findViewById(R.id.fragmentTypeText2);
-                                typeText2.setText(pokemonSelected.getTypes()[1].name());
-                            }
-
-                            ImageView imageView = view1.findViewById(R.id.fragmentPokemonImage);
-                            Picasso.get().load(pokemonSelected.getImageURL()).into(imageView);
-
-                            Spinner abilitySelectSpinner = view1.findViewById(R.id.fragmentPokemonAbilitySpinner);
-                            ArrayAdapter<String> abilityAdapter = new ArrayAdapter<>(
-                                    view1.getContext(),
-                                    android.R.layout.simple_spinner_item
-                            );
-                            abilityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                            for (String ability: pokemonSelected.getAbilities()) {
-                                abilityAdapter.add(
-                                        WordUtils.capitalize(
-                                                ability.replaceAll("-", " ")
-                                        )
-                                );
-                            }
-                            abilitySelectSpinner.setAdapter(abilityAdapter);
+                        TextView typeText1 = view1.findViewById(R.id.fragmentTypeText1);
+                        typeText1.setText(pokemonSelected.getTypes()[0].name());
+                        if (pokemonSelected.getTypes()[1] != null) {
+                            TextView typeText2 = view1.findViewById(R.id.fragmentTypeText2);
+                            typeText2.setText(pokemonSelected.getTypes()[1].name());
                         }
 
-                        return true;
+                        ImageView imageView = view1.findViewById(R.id.fragmentPokemonImage);
+                        Picasso.get().load(pokemonSelected.getImageURL()).into(imageView);
+
+                        Spinner abilitySelectSpinner = view1.findViewById(R.id.fragmentPokemonAbilitySpinner);
+                        ArrayAdapter<String> abilityAdapter = new ArrayAdapter<>(
+                            view1.getContext(),
+                            android.R.layout.simple_spinner_item
+                        );
+                        abilityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        for (String ability: pokemonSelected.getAbilities()) {
+                            abilityAdapter.add(
+                                WordUtils.capitalize(
+                                    ability.replaceAll("-", " ")
+                                )
+                            );
+                        }
+                        abilitySelectSpinner.setAdapter(abilityAdapter);
                     }
-                    return false;
+
+                    return true;
                 }
+            return false;
+            }
         );
 
         return view1;
