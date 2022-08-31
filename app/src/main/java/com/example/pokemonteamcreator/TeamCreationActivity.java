ca2c80648a1e489845627f84db3a1db2c6ec8918
JoshGill3882 <joshgill2013@gmail.com>
2022-08-31 11:35:03 +0100

    "Save Team" button in place with blank method

--- app/src/main/java/com/example/pokemonteamcreator/TeamCreationActivity.java
+++ app/src/main/java/com/example/pokemonteamcreator/TeamCreationActivity.java
@@ -1,8 +1,12 @@
 package com.example.pokemonteamcreator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.pokemonteamcreator.data.Pokemon;
 
 public class TeamCreationActivity extends AppCompatActivity {
 
@@ -10,5 +14,9 @@ public class TeamCreationActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team_creation);
+    }
+
+    public void saveTeamButton(View view) {
+
     }
 }
